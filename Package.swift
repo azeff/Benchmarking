266b7b671b6f8ca56e447b7ad7725e21099a3bc8
Evgenii Kazakov <azeffin@gmail.com>
2020-05-02 10:56:50 +0300

    Update ArgumentParser version to 0.0.5

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "Benchmarking", targets: ["Benchmarking", "BenchmarkIPC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.4"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.5"),
     ],
     targets: [
         .target(name: "BenchmarkIPC", path: "BenchmarkIPC"),
